af8341da3c49b203a0650a4a9b3e73925d11a880
Vincent Noel <vincent.noel@gmail.com>
2012-04-01 18:46:30 +0200

    Add Antarctic Peninsula map

--- mymaps.py
+++ mymaps.py
@@ -12,3 +12,8 @@ def northpole(boundinglat=60):
 
     bm = Basemap(projection='npstere', boundinglat=boundinglat, lon_0=0, resolution='i')
     return bm
+    
+def peninsula():
+    
+    bm = Basemap(projection='lcc', lat_0=-70, lon_0=-60, width=2e6, height=2e6, resolution='i')
+    return bm
