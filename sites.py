0f80171e14f17df7619cbcbd2e893b8f16dfb0a6
Vincent Noel <vincent.noel@gmail.com>
2014-03-18 18:31:54 -0400

    don't need terminator in paths list

--- sites.py
+++ sites.py
@@ -15,14 +15,16 @@ import glob
 
 hostname = socket.gethostname()
 if hostname == 'access.icare.univ-lille1.fr':
-    l1dir = ('/DATA/LIENS/CALIOP/CAL_LID_L1.v3.01', None)
+    # ICARE
+    l1dir = ('/DATA/LIENS/CALIOP/CAL_LID_L1.v3.01',)
     l2dir = ('/DATA/LIENS/CALIOP/05kmCLay.v3.02',
              '/DATA/LIENS/CALIOP/05kmCLay.v3.01')
     l2adir = ('/DATA/LIENS/CALIOP/05kmALay.v3.01',)
 else:
-    l1dir = ('/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.30/',
-             '/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.02/',
-             '/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.01/')
+    # CLIMSERV
+    l1dir = ('/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.30',
+             '/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.02',
+             '/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.01')
     l2dir = ('/bdd/CALIPSO/Lidar_L2/05kmCLay.v3.02',
              '/bdd/CALIPSO/Lidar_L2/05kmCLay.v3.01',
              '/bdd/CALIPSO/Lidar_L2/05kmCLay.v2.01',
