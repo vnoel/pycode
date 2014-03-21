fix a few off-by-one errors

calipso.py
@@ -43,7 +43,7 @@ def _vector_average(v0, navg, missing=None, valid=None):
     if navg == 1:
         return v0
 
-    n = np.size(v0, 0) / navg
+    n = np.floor(1. * np.size(v0, 0) / navg)
     v = np.zeros(n)
     if valid is None:
         valid = np.ones_like(v0)
@@ -117,7 +117,7 @@ def _array_average(a0, navg, weighted=False, valid=None, missing=None):
         w = None
 
     # create averaged array a with number of averaged profiles n
-    n = np.size(a0, 0) / navg
+    n = np.floor(1. * np.size(a0, 0) / navg)
     a = np.zeros([n, np.size(a0, 1)])
     if valid is None:
         # si on n'a pas d'info sur les profils valides, on suppose qu'ils le sont tous
@@ -291,10 +291,10 @@ class Cal1(_Cal):
 
         return nprof
 
-    def utc_time(self, navg=30, idx=(0, -1)):
+    def utc_time(self, navg=30, idx=None):
         var = self.hdf.select('Profile_UTC_Time')
         time = var[:].squeeze()
-        if idx[0] > 0 and idx[1] > -1:
+        if idx is not None:
             time = time[idx[0]:idx[1]]
         # if self.valid_rms_profiles is not None:
         #     time = time[self.valid_rms_profiles]
@@ -325,7 +325,7 @@ class Cal1(_Cal):
             datetimes.append(profile_datetime)
         return np.array(datetimes)
 
-    def time(self, navg=30, idx=(0, -1)):
+    def time(self, navg=30, idx=None):
         """
         Reads time for CALIOP profiles, averaged on navg
         shape [nprof]
@@ -334,7 +334,8 @@ class Cal1(_Cal):
         """
         var = self.hdf.select('Profile_Time')
         time = var[:].squeeze()
-        time = time[idx[0]:idx[1]]
+        if idx is not None:
+        	time = time[idx[0]:idx[1]]
 
         if navg > 1:
             n = np.size(time, 0) / navg
@@ -347,7 +348,7 @@ class Cal1(_Cal):
 
         return time
 
-    def coords(self, navg=30, idx=(0, -1)):
+    def coords(self, navg=30, idx=None):
         """
         Reads coordinates for CALIOP profiles, averaged on navg
         shape [nprof]
@@ -390,7 +391,7 @@ class Cal1(_Cal):
             atbstd = self.atb[prof, :]
         return atbstd
 
-    def atb(self, navg=30, prof=None, idx=(0, -1)):
+    def atb(self, navg=30, prof=None, idx=None):
         """
         Reads the Attenuated Total Backscatter 532nm from CALIOP file
         shape [nprof, nz]
@@ -435,7 +436,7 @@ class Cal1(_Cal):
         p = self._read_var("Pressure", navg, idx=idx)
         return p
 
-    def mol(self, navg=30, prof=None, idx=(0, -1)):
+    def mol(self, navg=30, prof=None, idx=None):
         """
         Reads the ancillary molecular number density from CALIOP file
         shape [nprof, nlevels]
@@ -445,7 +446,7 @@ class Cal1(_Cal):
             mol = mol[prof, :]
         return mol
 
-    def temperature(self, navg=30, idx=(0, -1)):
+    def temperature(self, navg=30, idx=None):
         """
         Reads the ancillary temperature field in degC from CALIOP file
         shape [nprof, nlevels]

tests/test_calipso.py
@@ -9,7 +9,7 @@ test_l1_file='/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.30/2013/2013_04_07/CAL_LID_L1-
 
 nz = 583
 nz_met = 33
-nprof = 56189
+nprof = 56190
 
 @pytest.fixture(scope="module")
 def cal1file():
@@ -23,20 +23,25 @@ def test_date(cal1file):
     assert cal1file.month == 4
     assert cal1file.day == 7
     
+
+def load_vectors(cal1file, navg=1):
+    
+    lon, lat = cal1file.coords(navg=navg)
+    time = cal1file.time(navg=navg)
+    utc  = cal1file.utc_time(navg=navg)
+    elev = cal1file.surface_elevation(navg=navg)
+    return lon, lat
+
     
 def test_nprof(cal1file):
     
-    lon, lat = cal1file.coords(navg=1)
-    time = cal1file.time(navg=1)
-    ut  = cal1file.utc_time(navg=1)
-    assert len(lat)==nprof
-    
-   
-def test_nprof_avg(cal1file, navg=30):
-    
-    lon, lat = cal1file.coords(navg=navg)
-    assert len(lat)==np.floor(1.*nprof/navg)
-    
+    vectors = load_vectors(cal1file,navg=1)
+    for vector in vectors:
+        assert len(vector)==nprof
+    vectors = load_vectors(cal1file, navg=30)
+    for vector in vectors:
+        assert len(vector)==np.floor(1.*nprof/30)
+        
     
 def test_nz(cal1file):
     