d3f08b173c6da85ca7b69177a4cf10bfe11ccfff
Vincent Noel <vincent.noel@gmail.com>
2014-04-20 13:21:19 +0200

    add run checks

--- localpaths.py
+++ localpaths.py
@@ -17,7 +17,7 @@ if hostname == 'access.icare.univ-lille1.fr':
     l2dir = ('/DATA/LIENS/CALIOP/05kmCLay.v3.02',
              '/DATA/LIENS/CALIOP/05kmCLay.v3.01')
     l2adir = ('/DATA/LIENS/CALIOP/05kmALay.v3.01',)
-else:
+elif hostname.endswith('ipsl.polytechnique.fr'):
     # CLIMSERV
     l1dir = ('/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.30',
              '/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.02',
@@ -26,3 +26,16 @@ else:
              '/bdd/CALIPSO/Lidar_L2/05kmCLay.v3.01',
              '/bdd/CALIPSO/Lidar_L2/05kmCLay.v2.01',
              '/bdd/CALIPSO/Lidar_L2/05kmCLay.v2.01')
+else:
+    l1dir = None
+    l2dir = None
+
+
+def main():
+    print ('CALIPSO L1 data dir : ',l1dir)
+    print ('CALIPSO L2 data dir : ',l2dir)
+
+
+if __name__ == '__main__':
+    import plac
+    plac.call(main)
