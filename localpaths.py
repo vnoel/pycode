a50776f2659fcba147d2c5ca60e8b6ac7d388f7a
Vincent Noel <vincent.noel@gmail.com>
2014-04-20 13:29:30 +0200

    refactor a bit for new data files

--- localpaths.py
+++ localpaths.py
@@ -10,14 +10,22 @@ import socket
 
 hostname = socket.gethostname()
 
+icare_id = 'icare.univ-lille1.fr'
+climserv_id = 'ipsl.polytechnique.fr'
+
+# known data directories
+l1dir = None        # CALIOP L1
+l2dir = None        # CALIOP L2
+eradir = None       # ECMWF
+
 # host-dependent paths pointing to CALIOP level 1 and level 2 data
-if hostname.endswith('icare.univ-lille1.fr'):
+if hostname.endswith(icare_id):
     # ICARE
     l1dir = ('/DATA/LIENS/CALIOP/CAL_LID_L1.v3.01',)
     l2dir = ('/DATA/LIENS/CALIOP/05kmCLay.v3.02',
              '/DATA/LIENS/CALIOP/05kmCLay.v3.01')
     l2adir = ('/DATA/LIENS/CALIOP/05kmALay.v3.01',)
-elif hostname.endswith('ipsl.polytechnique.fr'):
+elif hostname.endswith(climserv_id):
     # CLIMSERV
     l1dir = ('/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.30',
              '/bdd/CALIPSO/Lidar_L1/CAL_LID_L1.v3.02',
@@ -26,14 +34,16 @@ elif hostname.endswith('ipsl.polytechnique.fr'):
              '/bdd/CALIPSO/Lidar_L2/05kmCLay.v3.01',
              '/bdd/CALIPSO/Lidar_L2/05kmCLay.v2.01',
              '/bdd/CALIPSO/Lidar_L2/05kmCLay.v2.01')
-else:
-    l1dir = None
-    l2dir = None
 
+# ECMWF data
+if hostname.endswith(icare_id):
+    eradir = '/bdd/OPERA/NETCDF/GLOBAL_1125/4xdaily/'
+    
 
 def main():
     print ('CALIPSO L1 data dir : ',l1dir)
     print ('CALIPSO L2 data dir : ',l2dir)
+    print ('ECMWF data dir : ', eradir)
 
 
 if __name__ == '__main__':
