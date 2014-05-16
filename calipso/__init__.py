55a01b73d0af272c1d1687269c59238840258eb3
Vincent Noel <vincent.noel@gmail.com>
2014-05-16 12:37:48 +0200

    rename level1 and 2 modules, moved stuff in __init__.py

---
+++ calipso/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# V. Noel 2008-2014
+# LMD/CNRS
+
+"""
+Read CALIPSO lidar data files
+level 1 and level 2.
+
+This module is a container for Cal1 and Cal2 classes that do the actual file reading.
+"""
+
+
+from level1 import Cal1
+from level2 import Cal2
