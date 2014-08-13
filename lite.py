4ce3613cd538e368b1f76bfff3ccb06c7b23f5d1
Vincent Noel <vincent.noel@gmail.com>
2014-08-13 13:50:50 +0200

    redesigned the atb vs photons distinction

--- lite.py
+++ lite.py
@@ -25,6 +25,18 @@ The script can also be called on a LITE data file for quick checks:
     MetDate:  0 18 23 3 13
     lat, lon:  45.1957 120.065
     Number of profiles:  (572, 3000)
+    
+Initially, only the photon counts are available. Backscatter fields ("atb") are empty after reading a LITE data file. 
+To fill them, they need to be calibrated:
+
+    >>> l.calibrate_atb_to(532, 1e-4, between_altitudes=[20,22])
+    
+This will calibrate the atb at 532nm by bringing the average value between 20 and 22 km to 1e-4.
+Instead of a single value, a vector of calibration values can be passed (length = number of profiles in the file).
+
+For convenience, backscatter can also be calibrated from a vector of molecular backscatter values
+
+    >>> l.calibrate_atb_from_molecular(532, average_mol_between_20_and_22, altcal=[20,22])
 
 '''
 
@@ -165,60 +177,76 @@ header = np.dtype( [
                 
                 ] )
     
+wv_keys = {355:'profile355', 532:'profile532', 1064:'profile064'}
 
 class LITE(object):
     
-    def __init__(self, filename):
+    def __init__(self, filename, altitude_bottom_up=True):
         
         fid = open(filename, 'r')
         self.rawdata = np.fromfile(fid, dtype=header, count=-1)
         fid.close()
-        self.rawaltitude = np.linspace(40, -4.985, 3000)
-        self.altitude = self.rawaltitude[::-1]
+        self.altitude = np.linspace(40, -4.985, 3000)
         self.nprof = self.rawdata['profile355'].shape[0]
-        self.photons = dict()
         self.latitude = self.rawdata['latitude']
         self.longitude = self.rawdata['longitude']
-        # dimensions [nprof, altitude]
-        self.photons[355] = self.rawdata['profile355'][:,::-1]
-        self.photons[532] = self.rawdata['profile532'][:,::-1]
-        self.photons[1064] = self.rawdata['profile064'][:,::-1]
-        
+
+        if altitude_bottom_up:
+            self.altitude = self.altitude[::-1]
+            for field in 'profile355', 'profile532', 'profile064':
+                self.rawdata[field] = self.rawdata[field][:,::-1]
+
         self.datetimes = []
         for i in np.r_[0:self.nprof]:
             date = datetime(1994,1,1) + timedelta(days=self.rawdata['gmtday'][i]-1)
             fulldate = datetime(date.year, date.month, date.day, self.rawdata['gmthour'][i], self.rawdata['gmtmin'][i], self.rawdata['gmtsec'][i], self.rawdata['gmthund'][i]*10000)
             self.datetimes.append(fulldate)
-                
-    def calibrate_atb(self, mol_atb, altcal=[20,22]):
+
+        self.atb = dict()
+        self.atb[355] = None
+        self.atb[532] = None
+        self.atb[1064] = None            
+    
+    def calibrate_atb_to(self, wv, mol_atb, between_altitudes=[20,24], horiz_avg=10):
 
         '''
         calibrate the photon counts as ATB, by bringing the average beween altcal[0] and altcal[1] to the values in mol_atb
         mol_atb is either a single value or a vector with length nprof
+        
+        horiz_avg is the optional window size for the rolling average to smooth out the calibration factor.
+        
+        the function returns the calibration factor.
         '''
         
-        idx = (self.altitude >= altcal[0]) & (self.altitude < altcal[1])
-        for data in self.photons:
-            reference = np.mean(self.photons[data][:,idx], axis=1)
-            cal_factor = mol_atb / reference
-            self.photons[data] = (self.photons[data].T * cal_factor).T
+        idx = (self.altitude >= between_altitudes[0]) & (self.altitude < between_altitudes[1])
+        reference = np.mean(self.rawdata[wv_keys[wv]][:,idx], axis=1)
+        if horiz_avg > 1:
+            reference = _rolling_average_padded(reference, horiz_avg)
+        cal_factor = mol_atb / reference
+        self.atb[wv] = (self.rawdata[wv_keys[wv]].T * cal_factor).T
+        
+        return cal_factor
         
     def vertical_downsample(self, new_z_step):
         '''
-        vertically downsample the photon counts. E.g go from the initial resolution of 30m to 100m.
+        vertically downsample the backscatter profiles. E.g go from the initial resolution of 30m to 100m.
         Does not upsample (e.g. resolution of 10m)
         Correctly takes into account partial boundaries between origin and target binsizes.
         
         new_z_step in km
+        
+        backscatter needs to be calibrated (using calibrate_atb_to) before being downsampled
         '''
         
         assert new_z_step > (self.altitude[1] - self.altitude[0]), 'new_z_step cannot be smaller than 30m'
         z2 = np.r_[-1.:40:new_z_step]
-        for data in self.photons:
-            data2 = np.zeros([self.photons[data].shape[0], z2.shape[0]])
+        for wv in self.atb:
+            if self.atb[wv] is None:
+                continue
+            data2 = np.zeros([self.atb[wv].shape[0], z2.shape[0]])
             for i in np.r_[0:data2.shape[0]]:
-                data2[i,:] = _downsample_profile(self.altitude, self.photons[data][i,:], z2)
-            self.photons[data] = data2
+                data2[i,:] = _downsample_profile(self.altitude, self.atb[wv][i,:], z2)
+            self.atb[wv] = data2
         self.altitude = z2    
                 
     def describe(self, prof=0):
@@ -233,7 +261,7 @@ class LITE(object):
         print '\tMetDate: ', self.rawdata['metday'][prof], self.rawdata['methour'][prof], self.rawdata['metmin'][prof], self.rawdata['metsec'][prof], self.rawdata['methund'][prof]
         print '\tlat, lon: ', self.rawdata['latitude'][prof], self.rawdata['longitude'][prof]
         
-    def plot_profiles(self, wv=355):
+    def plot_photon_profiles(self, wv=355):
         
         import matplotlib.pyplot as plt
         import matplotlib.dates as mdates
@@ -241,7 +269,7 @@ class LITE(object):
         ndates = mdates.date2num(self.datetimes)
         fig = plt.figure(figsize=[15,5])
         ax = plt.gca()
-        plt.pcolormesh(ndates, self.altitude, self.photons[wv].T)
+        plt.pcolormesh(ndates, self.altitude, self.rawdata[wv_keys[wv]].T)
         ax.xaxis.axis_date()
         plt.ylabel('Altitude')
         fig.autofmt_xdate()
@@ -267,6 +295,7 @@ if __name__ == '__main__':
 
 # utility functions
 
+
 def _downsample_profile(z, prof, z2):
     prof2 = np.zeros_like(z2)
     for i in np.r_[0:z2.shape[0]-1]:
@@ -283,3 +312,12 @@ def _downsample_profile(z, prof, z2):
         prof2[i] = np.average(prof[idx], weights=weights)
     return prof2
 
+
+def _rolling_average_padded(vector, window):
+    import pandas as pd
+    d = pd.Series(vector)
+    d = pd.rolling_mean(d, window)
+    vector = d.values
+    vector[0:window] = vector[window]
+    vector[-window:] = vector[-window]
+    return vector
